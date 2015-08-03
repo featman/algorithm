c604998ac989b2fb0e6c6676ab3b07fdcad74681
songzhicheng <songzhicheng2007@gmail.com>
2015-08-03 18:16:39 +0800

    update

---
+++ linuxShell/GenralShell.sh
@@ -0,0 +1,19 @@
+:<<!/*
+ * GenralShell.c
+ *
+ *  Created on: 2015年8月3日
+ *      Author: Song Zhi-Cheng
+ */
+！
+#!/bin/sh
+
+#统计文章中出现的字数，行数
+ls |wc -l	#当前目录下文件的个数
+cat abc.txt |wc #统计abc.txt文件中的行数、字数、以及所占字节数
+
+
+
+
+
+
+
--- linuxShell/linuxShell.sh
+++ linuxShell/linuxShell.sh
@@ -1,13 +1,5 @@
-/*
- ============================================================================
- Name        : linuxShell.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 
+:<<!
 /*Transpose File
 Given a text file file.txt, transpose its content.
 You may assume that each row has the same number of columns and each field is separated by the ' ' character.
@@ -21,5 +13,7 @@ Output the following:
 name alice ryan
 age 21 30
 */
+£¡
 #!/bin/sh
+ls -l |wc -l
 

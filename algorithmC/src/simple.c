4d9b05f2f63c0b66b17afc7246b501d96ec615dd
songzhicheng <songzhicheng2007@gmail.com>
2015-08-03 08:13:29 +0800

    add

---
+++ algorithmC/src/simple.c
@@ -0,0 +1,19 @@
+/*
+ * SingNumber.c
+ *
+ *  Created on: 2015Äê8ÔÂ2ÈÕ
+ *      Author: Song Zhi-Cheng
+ */
+
+
+
+
+int singleNumber(int* nums, int numsSize) {
+
+	int i,res = 0;
+	for(i=0;i<numsSize;i++){
+		res ^=nums[i];
+	}
+	return res;
+
+}
